de0cc68c75f45bd7ae8e89810ab590f004b942e3
Muhammad Bonn <118687900+muhammadbonn@users.noreply.github.com>
2026-04-26 22:57:32 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,45 @@
+provider "aws" {
+  region = var.aws_region
+}
 
+resource "aws_security_group" "wordpress_sg" {
+  name        = "wordpress-sg"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = file("/home/muhammad_bonn/.ssh/id_rsa.pub") 
+}
+
+resource "aws_instance" "wordpress_server" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.wordpress_sg.id]
+
+  tags = {
+    Name = "WordPress-Server"
+  }
+}
