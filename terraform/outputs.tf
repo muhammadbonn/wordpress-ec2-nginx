ba1735c951e90d1e2aef6b0b9b8723478b6b87f5
Muhammad Bonn <118687900+muhammadbonn@users.noreply.github.com>
2026-04-29 21:11:06 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,19 @@
+# --- Infrastructure Outputs ---
+
+# The Public IP address of the newly created EC2 instance
 output "public_ip" {
-  description = "Public IP of the EC2 instance"
+  description = "The public IP address of the WordPress server"
   value       = aws_instance.wordpress_server.public_ip
+}
+
+# The Direct URL to access your WordPress site
+output "wordpress_url" {
+  description = "The URL to access the WordPress website"
+  value       = "http://${aws_instance.wordpress_server.public_ip}"
+}
+
+# SSH Command helper for quick maintenance access
+output "ssh_command" {
+  description = "Command to SSH into the server"
+  value       = "ssh -i ~/.ssh/id_rsa ubuntu@${aws_instance.wordpress_server.public_ip}"
 }
